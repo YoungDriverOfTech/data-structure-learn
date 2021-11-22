6f43c9764c89a7851478d08729eea8535a168991
liang <xiaoleliang045@gmail.com>
2021-11-22 23:15:20 +0900

    update comment

--- src/main/java/com/data/structure/enhanceStackDemo/EnhanceStackTest.java
+++ src/main/java/com/data/structure/enhanceStackDemo/EnhanceStackTest.java
@@ -4,7 +4,6 @@ public class EnhanceStackTest {
     public static void main(String[] args) {
         EnhanceStack stack = new EnhanceStack(4);
         stack.push(1);
-        //System.out.println(stack.peek());
         stack.push(2);
         stack.push(3);
         stack.push("abc");
